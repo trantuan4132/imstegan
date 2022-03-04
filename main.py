e48d7e2e59ce54ee1628c4230ed183610612d6cf
20toduc01 <20toduc01@gmail.com>
2022-03-04 16:47:31 +0700

    Some more refactor and use cv2 instead of skimage

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
-from skimage import io
 import matplotlib.pyplot as plt
+import cv2
 
-import algorithms
+import steganography
 
 def parse_args():
     parser = argparse.ArgumentParser(description='LSB embedding and extraction')
@@ -16,9 +16,8 @@ def parse_args():
 
 def main():
     args = parse_args()
-    image = io.imread(args.image_path)
-    message = args.message
-    algorithm = getattr(algorithms, args.algorithm_name)
+    image = cv2.imread(args.image_path)
+    algorithm = getattr(steganography, args.algorithm_name)
     key = args.key
     extract = args.extract
 
@@ -27,8 +26,8 @@ def main():
         print(message)
     else:
         image = algorithm(key=key).embed_image(image, message)
-        io.imsave(args.output_path, image)
-        plt.imshow(image)
+        cv2.imwrite(args.output_path, image)
+        plt.imshow(image[:, :, ::-1])
         plt.show()
 
 if __name__ == '__main__':
