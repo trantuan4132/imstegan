e7045f5c8c4cd870cea1da4f4cad0561250b4560
Đức Văn <20toduc01@gmail.com>
2022-03-09 18:23:01 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='imstegan',
+   version='0.0.1',
+   description='An awesome image steganography library',
+   author='Duc To, Tuan Tran and Viet Nguyen',
+   author_email='',
+   url='',
+   packages=['imstegan'],
+   install_requires=['Pillow', 'numpy', 'numba'],
+)
